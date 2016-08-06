a16f58e3ac165462815c4565f1501ad42c8db74a
lizdenhup <lizdenhup@gmail.com>
2016-08-05 19:11:57 -0500

    working on board class

---
+++ lib/board.rb
@@ -0,0 +1,33 @@
+class Board
+
+  def initialize
+    @board = Array.new(10) {Array.new(10,0)}
+  end
+
+  def living_cells(x,y)
+
+  end
+
+  def cell_is_alive?
+
+  end 
+
+  def neighborhood
+#    neighborhood = 
+  end 
+
+
+  def born
+# a cell is born if it has 3 neighbors
+  end 
+
+  def lives_on
+# a cell lives on if it has 2 or 3 neighbors
+  end 
+
+  def dies
+
+  end 
+
+end 
+
--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,11 @@
 class Game
 
+  def initial_patterns
+
+  end 
+
+  def display_board
+
+  end 
+
 end 
